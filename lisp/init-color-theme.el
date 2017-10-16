66a33f90572ab3461b316540f6355fa33dc385a2
mengqp <meng_qingpu@126.com>
2017-10-16 10:34:26 +0800

    use zenburn theme

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -12,11 +12,40 @@
 ;; (require 'spacemacs-theme)
 ;; (load-theme 'spacemacs-dark t)
 
-(use-package cyberpunk-theme
+;; (use-package cyberpunk-theme
+;;   :ensure t
+;;   :config
+;;   (add-hook 'after-init-hook
+;; 	    (lambda () (load-theme 'cyberpunk t)))
+;;   )
+
+(use-package zenburn-theme
   :ensure t
   :config
-  (add-hook 'after-init-hook
-	    (lambda () (load-theme 'cyberpunk t)))
+  (load-theme 'zenburn t)
   )
+
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config
+;; ;; Global settings (defaults)
+;; (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;       doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; ;; may have their own settings.
+;; (load-theme 'doom-one t)
+
+;; ;; Enable flashing mode-line on errors
+;; (doom-themes-visual-bell-config)
+
+;; ;; Enable custom neotree theme
+;; ;; (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+;; ;; Corrects (and improves) org-mode's native fontification.
+;; (doom-themes-org-config)
+
+;;   )
+
 
 (provide 'init-color-theme)
