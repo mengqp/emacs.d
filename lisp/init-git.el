6ad3f0242923ce06b0a29c9b894f8267ad314bd0
mengqp <meng_qingpu@126.com>
2017-11-04 11:30:21 +0800

    修改git-gutter 的修改“  ”改为“==”

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -22,11 +22,11 @@
   (add-hook 'org-mode-hook 'git-gutter-mode)
   :config
   (custom-set-variables
-   '(git-gutter:modified-sign "  ") ;; two space
+   '(git-gutter:modified-sign "==") ;; two space
    '(git-gutter:added-sign "++")    ;; multiple character is OK
    '(git-gutter:deleted-sign "--"))
 
-  (set-face-background 'git-gutter:modified "purple") ;; background color
+  (set-face-background 'git-gutter:modified "yellow") ;; background color
   (set-face-foreground 'git-gutter:added "green")
   (set-face-foreground 'git-gutter:deleted "red")
   )
