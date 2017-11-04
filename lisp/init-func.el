ee048e7a225e3ef345a73f691bd837cb68fd9f7b
mengqp <meng_qingpu@126.com>
2017-11-04 11:32:39 +0800

    删除无用的功能

--- lisp/init-func.el
+++ lisp/init-func.el
@@ -5,11 +5,11 @@
   (find-file "~/.emacs.d/init.el")
     )
 
-(defun mengqp/find-readme
-  "find root/readme.org in the project"
-  (interactive)
-  (projectile-find-file "readme.org")
-    )
+;; (defun mengqp/find-readme
+;;   "find root/readme.org in the project"
+;;   (interactive)
+;;   (projectile-find-file "readme.org")
+;;     )
 
 (defun mengqp/open-init-package-file ()
   "open init package file to config"
