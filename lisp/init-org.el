05271d162b1915aadbf26f22f9727432af3e0fe8
mengqp <meng_qingpu@126.com>
2017-12-08 15:23:43 +0800

    develop 去除orgminmap 库用稳定库

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -182,10 +182,10 @@
     (insert (number-to-string num))
     )
 
-  (use-package org-mind-map
-    :defer t
-    :ensure t
-    )
+  ;; (use-package org-mind-map
+  ;;   :defer t
+  ;;   :ensure t
+  ;;   )
 
   ;; (define-key evil-normal-state-map (kbd "T") 'mengqp/evil-org-insert-todo-at-end)
   ;; (define-key evil-normal-state-map (kbd "t") 'org-todo)
