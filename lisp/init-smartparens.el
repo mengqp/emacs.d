be003e2fbf3494c3a025e14078089f91a97ccef3
mengqp <meng_qingpu@126.com>
2018-03-26 11:47:55 +0800

    develop 更新smartparent

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,18 +1,46 @@
+;;; init-smartparens.el --- smartparens -*- coding: utf-8-unix -*-
 
+;;; Copyright © 2018 - 2018 mengqp.
+
+;; Author: mengqp
+;; URL:
+;; Version:0.0.1
+;; Keywords:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
 (use-package smartparens
   :ensure t
-  :diminish smartparens-mode
+  :diminish smartparens-mode smartparens-global-mode
   :config
   ;; enable the default configuration
   (use-package smartparens-config)
   (smartparens-global-mode t)
 
   )
-;; (require 'smartparens)
-
-;;  ;; enable the default configuration
-;; (require 'smartparens-config)
-
-;; (smartparens-global-mode t)
-
 (provide 'init-smartparens)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-smartparens.el ends here
