562ac1e65ff86be9c0a158de9dee0b5b6cafb526
mengqp <meng_qingpu@126.com>
2018-05-04 20:03:52 +0800

    develop 更换emacschina 源

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -41,13 +41,16 @@ But you may use safer HTTPS instead.")
 
 
 (setq package-archives '(
-			 ;; ("melpa-cn-stable" . "http://elpa.emacs-china.org/melpa-stable/")
-			 ("melpa-cn" . "https://elpa.zilongshanren.com/melpa/")
-			  ("org-cn"   . "https://elpa.zilongshanren.com/org/")
-                          ("gnu-cn"   . "https://elpa.zilongshanren.com/gnu/")
-                          ;; ("melpa" . "https://melpa.org/packages/")
-                          ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
-                          ))
+                         ;; ("melpa-cn-stable" . "http://elpa.emacs-china.org/melpa-stable/")
+                         ;; ("melpa-cn" . "https://elpa.zilongshanren.com/melpa/")
+                         ;;  ("org-cn"   . "https://elpa.zilongshanren.com/org/")
+                         ;; ("gnu-cn"   . "https://elpa.zilongshanren.com/gnu/")
+                         ("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+                         ("org-cn"   . "http://elpa.emacs-china.org/org/")
+                         ("gnu-cn" . "http://elpa.emacs-china.org/gnu/")
+                         ;; ("melpa" . "https://melpa.org/packages/")
+                         ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ))
 
 
 
