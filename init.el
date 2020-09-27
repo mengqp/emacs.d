c9a6a0258ad2208dc0f99b45c89be994c45174c6
mengqp <meng_qingpu@126.com>
2020-09-27 18:22:57 +0800

    clean code

--- init.el
+++ init.el
@@ -1,23 +1,23 @@
 ;;; init.el --- Config for mengqp -*- coding: utf-8-unix -*-
 ;;; Commentary:
 ;;; Code:
+
 ;;Speed up startup
 (defvar default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
-(setq gc-cons-threshold 4000000)
-;; (setq gc-cons-threshold 80000000)
-;; (add-hook 'emacs-startup-hook
-;;           (lambda ()
-;;             "Restore defalut values after init."
-;;             (setq file-name-handler-alist default-file-name-handler-alist)
-;;             (setq gc-cons-threshold 800000)
-;;             (if (boundp 'after-focus-change-function)
-;;                 (add-function :after after-focus-change-function
-;; 			      (lambda ()
-;;                                 (unless (frame-focus-state)
-;;                                   (garbage-collect))))
-;; 	      (add-hook 'focus-out-hook 'garbage-collect))))
-
+;; (setq gc-cons-threshold 4000000)
+(setq gc-cons-threshold 80000000)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            "Restore defalut values after init."
+            (setq file-name-handler-alist default-file-name-handler-alist)
+            (setq gc-cons-threshold 800000)
+            (if (boundp 'after-focus-change-function)
+                (add-function :after after-focus-change-function
+			      (lambda ()
+                                (unless (frame-focus-state)
+                                  (garbage-collect))))
+	      (add-hook 'focus-out-hook 'garbage-collect))))
 
 ;; Load path
 ;; Optimize: Force "lisp"" and "site-lisp" at the head to reduce the startup time.
@@ -38,18 +38,8 @@
 
 (update-load-path)
 
-
-
-;; @see https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
-;; Normally file-name-handler-alist is set to
-;; (("\\`/[^/]*\\'" . tramp-completion-file-name-handler)
-;; ("\\`/[^/|:][^/|]*:" . tramp-file-name-handler)
-;; ("\\`/:" . file-name-non-special))
-;; Which means on every .el and .elc file loaded during start up, it has to runs those regexps against the filename.
 (let ((file-name-handler-alist nil))
   ;; (require 'init-benchmarking)
-
-
   (require 'init-const)
   (require 'init-autoload)
   (require 'cl-lib)
@@ -65,14 +55,6 @@
   (require 'init-theme)
   (require 'init-cnfonts)
   ;; (require 'init-layout)
-  ;; (require 'init-dumb-jump)
-  ;; (require 'init-hydra)
-
-  ;; (when *linux*
-  ;; (require 'init-abbrev)
-  ;; (require 'init-spell)
-  ;;(require 'init-dap)
-  ;; )
   (require 'init-im)
 
   ;;tool
@@ -91,41 +73,20 @@
   (require 'init-func)
   (require 'init-keymap)
 
-  ;; (require 'init-shell)
-
   (require 'init-plantuml)
-  ;; (require 'init-ycmd)
   (require 'init-gdb)
-  ;; (require 'init-leecode)
-
-  ;; (require 'init-cscope)
-
   ;;
-  ;; (require 'init-realgud)
   (require 'init-cc)
   (require 'init-cmake)
   (require 'init-org)
   (require 'init-python)
   (require 'init-css)
   (require 'init-markdown)
-  ;; (require 'init-web)
   (require 'init-php)
 
   )
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-;; enable just-in-time native compilation
-;; (setq comp-deferred-compilation nil)
-
-;; native-compile all Elisp files under a directory
-;; (native-compile-async "~/.emacs.d/elpa/" 'recursively)
-;; (native-compile-async "~/.emacs.d/site-lisp/" 'recursively)
-
-
-;; (when *win64*
-;;   (server-start)
-;;   )
 
 
 ;;; init.el ends here
